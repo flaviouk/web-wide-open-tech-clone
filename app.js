7d825a16a071415372cb53b6b452d5841efcbb8a
flaviocarvalho <flavio046@hotmail.com>
2016-05-19 15:44:50 -0500

    Finish Project + bug fixes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-$(window).on('scroll', function(){
-	var scroll = $(window).scrollTop();
-	if(scroll < 100){
-		$('.navbar-default').removeClass('scrolling');
-		$('.nav-items').removeClass('scrolling-text');
-		$('.logo').attr("src","http://wideopentech.com/themes/wot-2013-gm/_/img/wot-logo-whitex2.svg")
-	}else{
-		$('.navbar-default').addClass('scrolling');
-		$('.nav-items').addClass('scrolling-text');
-		$('.logo').attr("src","http://wideopentech.com/themes/wot-2013-gm/_/img/wot-logo-dark.svg")
-	}
+$(document).on('ready', function(){
+	$(window).on('scroll', function(){
+		var scroll = $(window).scrollTop();
+		if(scroll < 100){
+			$('.navbar-default').removeClass('scrolling');
+			$('.nav-items').removeClass('scrolling-text');
+			$('.logo').attr("src","http://wideopentech.com/themes/wot-2013-gm/_/img/wot-logo-whitex2.svg")
+		}else{
+			$('.navbar-default').addClass('scrolling');
+			$('.nav-items').addClass('scrolling-text');
+			$('.logo').attr("src","http://wideopentech.com/themes/wot-2013-gm/_/img/wot-logo-dark.svg")
+		}
+	});
 });
