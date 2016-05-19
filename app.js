297c61155fc6958d7d9cb535c92a63a54ccb67a8
flaviocarvalho <flavio046@hotmail.com>
2016-05-19 15:36:20 -0500

    Finished main page

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-$(window).scroll(function (event) {
+$(window).on('scroll', function(){
 	var scroll = $(window).scrollTop();
-	while(scroll > 100){
-		$('.navbar-default').css('background-color', 'white').css('color', 'black');
+	if(scroll < 100){
+		$('.navbar-default').removeClass('scrolling');
+		$('.nav-items').removeClass('scrolling-text');
+		$('.logo').attr("src","http://wideopentech.com/themes/wot-2013-gm/_/img/wot-logo-whitex2.svg")
+	}else{
+		$('.navbar-default').addClass('scrolling');
+		$('.nav-items').addClass('scrolling-text');
+		$('.logo').attr("src","http://wideopentech.com/themes/wot-2013-gm/_/img/wot-logo-dark.svg")
 	}
 });
